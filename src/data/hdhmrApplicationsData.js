ec16dc0770af12ed67f059b30aab557cc575f3f1
Joseph Pal <josephpal27@gmail.com>
2026-08-10 12:06:50 +0530

    Wudon Application Images Updated

--- src/data/hdhmrApplicationsData.js
+++ src/data/hdhmrApplicationsData.js
@@ -2,16 +2,16 @@ export const hdhmrApplicationsData = [
     {
         id: 1,
         title: "Kitchen Cabinets <br/> & Shutters",
-        image: "/images/wudon/applications/1.avif",
+        image: "/images/wudon/applications/hdhmr/1.avif",
     },
     {
         id: 2,
         title: "Bathroom Vanity <br/> Units",
-        image: "/images/wudon/applications/2.avif",
+        image: "/images/wudon/applications/hdhmr/2.avif",
     },
     {
         id: 3,
         title: "Modular Furniture in <br/> Humid Climates",
-        image: "/images/wudon/applications/3.avif",
+        image: "/images/wudon/applications/hdhmr/3.avif",
     },
 ]
--- src/data/mdfApplicationsData.js
+++ src/data/mdfApplicationsData.js
@@ -2,16 +2,16 @@ export const mdfApplicationsData = [
     {
         id: 1,
         title: "Interior <br/> Door Panels",
-        image: "/images/wudon/applications/4.avif",
+        image: "/images/wudon/applications/mdf/1.avif",
     },
     {
         id: 2,
         title: "Shelves & <br/> Bookcases",
-        image: "/images/wudon/applications/5.avif",
+        image: "/images/wudon/applications/mdf/2.avif",
     },
     {
         id: 3,
         title: "Speaker Boxes & <br/> Acoustic Panels",
-        image: "/images/wudon/applications/6.avif",
+        image: "/images/wudon/applications/mdf/3.avif",
     },
 ]
