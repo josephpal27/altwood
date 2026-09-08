673b622b82051f364f9994795dea7b3921759827
Joseph Pal <josephpal27@gmail.com>
2026-09-08 22:58:32 +0530

    Contact Page init

---
+++ src/app/contact/page.js
@@ -0,0 +1,14 @@
+import ContactBanner from "@/components/contact/ContactBanner";
+
+export const metadata = {
+  title: "Contact Us - Altwood",
+  description: "",
+};
+
+export default function Contact() {
+  return (
+    <>
+      <ContactBanner />
+    </>
+  );
+}
---
+++ src/components/contact/ContactBanner.jsx
@@ -0,0 +1,42 @@
+"use client"
+
+import { motion } from "framer-motion";
+import { headingVariant } from "@/utils/animations";
+
+const ContactBanner = () => {
+    return (
+        <>
+            <section className="p-0 relative">
+                <img src="/images/banners/product-details-banner.avif" alt="Products Banner" loading="eager" className="
+                    w-full h-[45dvh] lg:h-auto object-cover
+                " />
+                <div className="
+                    absolute top-0 left-0 w-full h-full flex flex-col items-center justify-center
+                    px-[1rem] lg:px-[7%] bg-black/50
+                ">
+                    <motion.h1
+                        variants={headingVariant}
+                        initial="hidden"
+                        whileInView="visible"
+                        viewport={{ once: true }}
+                        className="
+                            text-white
+                            text-[2rem] sm:text-[2.2rem] lg:text-[3.4rem] xl:text-[3.7rem] 2xl:text-[4rem]
+                            mt-[7rem] lg:mt-[4.5rem]
+                        "
+                    >
+                        Contact Us
+                    </motion.h1>
+                    <p className="
+                        text-white mt-[0.6rem] lg:mt-[0.8rem] 
+                        w-full lg:w-[50%] text-center
+                    ">
+                        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                    </p>
+                </div>
+            </section>
+        </>
+    )
+}
+
+export default ContactBanner
