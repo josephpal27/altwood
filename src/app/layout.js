736c33663f7b841e677f3c05a15c4f08673d4f3c
josephpal27 <josephpal27@gmail.com>
2026-09-21 23:31:28 +0530

    Layout Updated

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,7 +5,7 @@ import "./globals.css";
 import Footer from "@/components/Footer";
 import CustomCursor from "@/components/CustomCursor";
 import SmoothScroll from "@/components/SmoothScroll";
-import ComingSoon from "@/components/ComingSoon";
+// import ComingSoon from "@/components/ComingSoon";
 
 const inter = Inter({
   subsets: ["latin"],
